15afa4450ed9b15b575faf2154f6e15c7821238b
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-10-03 16:05:30 +0500

    test task telegram bot 1.0.10 + Dockerfile

--- app/bot/config.py
+++ app/bot/config.py
@@ -1,2 +1,3 @@
 class BotToken:
-    TOKEN = "7606482390:AAGfQ0D5KCBpK-wFiw_yaQaO5KDXs_-c7j0"
+    # TOKEN = "7606482390:AAGfQ0D5KCBpK-wFiw_yaQaO5KDXs_-c7j0"
+    TOKEN = "7534232286:AAG93GvqXwUnpuBpGXKFAIaheIw-TDpnar4"
